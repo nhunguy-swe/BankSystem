0cfe0c19769bff9c200cc649e8ebf7bcd922f168
Nhunguy <munnguy@gmail.com>
2026-05-05 11:12:39 +0700

    test: finalize unit test logic with explicit Fail assert and early return

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -23,20 +23,25 @@ public class BankAccountTests {
     }
 
     [TestMethod]
-    public  void  Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange() {
-    // Arrange
-        double beginningBalance = 11.99; 
-        double  debitAmount  =  20.0;
-        BankAccount  account  =  new  BankAccount("Mr.  Bryan  Walton",  beginningBalance);
+    public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange() {
         
+        // Arrange
+        double beginningBalance = 11.99;
+        double debitAmount = 20.0;
+        BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
         // Act
-        try {
+        try
+        {
             account.Debit(debitAmount);
         }
-        catch  (System.ArgumentOutOfRangeException  e) {
-            
-        // Assert
-            StringAssert.Contains(e.Message,  BankAccount.DebitAmountExceedsBalanceMessage);
+        catch (System.ArgumentOutOfRangeException e)
+        {
+            // Assert
+            StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+            return;
         }
+        
+        Assert.Fail("The expected exception was not thrown.");
     }
 }
