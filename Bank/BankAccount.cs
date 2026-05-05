834c15fd8d1f185f4295eec60b5c37e3189a1f1a
Nhunguy <munnguy@gmail.com>
2026-05-05 11:05:12 +0700

    refactor: enhance exception handling with specific error messages and update unit tests

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -6,6 +6,9 @@ namespace BankAccountNS
         private readonly string m_customerName;
 
         private double m_balance;
+        
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than zero";
 
         private BankAccount() {
         }
@@ -25,11 +28,11 @@ namespace BankAccountNS
 
         public void Debit(double amount) {
             if (amount > m_balance) {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
 
             if (amount < 0) {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new System.ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
             m_balance -= amount; 
