1c9c199441106a29b8bb1b971a5c0909f7b29877
Nhunguy <munnguy@gmail.com>
2026-05-05 10:28:04 +0700

    refactor: cleanup code formatting

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -3,20 +3,21 @@ using BankAccountNS;
 namespace BankTests;
 
 [TestClass]
-public class BankAccountTests
-{
+public class BankAccountTests {
+    
     [TestMethod]
-    public void Debit_WithValidAmount_UpdatesBalance()
-    {
-// Arrange
-        double beginningBalance =
-            11.99; double debitAmount =
-            4.55; double expected = 7.44;
+    public void Debit_WithValidAmount_UpdatesBalance() {
+        
+// Arrange (Chuẩn bị)
+        double beginningBalance = 11.99; 
+        double debitAmount = 4.55; 
+        double expected = 7.44;
         BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
-// Act
-        account.Debit(debitAmount)
-            ;
-// Assert
+        
+// Act (Thực hiện)
+        account.Debit(debitAmount);
+        
+// Assert (Kiểm tra kết quả)
         double actual = account.Balance;
         Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
     }
